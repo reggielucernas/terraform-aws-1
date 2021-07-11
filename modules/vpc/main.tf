c7a70915cb161857ee456f139eb3746e55562c75
Reggie Lucernas <reggie.lucernas@gmail.com>
2021-07-11 00:27:22 -0700

    added stuff

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,3 @@
 resource "aws_vpc" "main" {
-    cidr_block = var.vpc_cidr
-    
-    tags = {
-        Name = "Base Platform VPC"
-    }
+  cidr_block = var.vpc_cidr
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,5 +1,23 @@
 variable "vpc_cidr" {
-    description = "The CIDR block for the VPC"
-    type = "string"
-    default = ""
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = ""
+}
+
+variable "public_subnet_cidr" {
+  description = "The CIDR block for the Public Subnet"
+  type        = string
+  default     = ""
+}
+
+variable "private_subnet_cidr" {
+  description = "The CIDR block for the Private Subnet"
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "Tags to set on the EC2 instance"
+  type        = map(string)
+  default     = { Name = "Base Platform VPC" }
 }
